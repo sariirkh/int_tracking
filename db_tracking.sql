5185eee505286d7db80ca779ee928e5ff39b633f
sariirkh <puspitasarirakhmatullah@gmail.com>
2020-09-25 10:12:48 +0700

    database

---
+++ db_tracking.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 25, 2020 at 05:12 AM
+-- Server version: 10.1.40-MariaDB
+-- PHP Version: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_tracking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lokasi`
+--
+
+CREATE TABLE `lokasi` (
+  `id_lokasi` int(11) NOT NULL,
+  `id_kendaraan` int(11) NOT NULL,
+  `nama_lokasi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lokasi`
+--
+
+INSERT INTO `lokasi` (`id_lokasi`, `id_kendaraan`, `nama_lokasi`) VALUES
+(1, 1, 'Mangli'),
+(12, 2, 'Jember'),
+(13, 3, 'Bangsal'),
+(14, 4, 'Tanggul');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `riwayat`
+--
+
+CREATE TABLE `riwayat` (
+  `id_riwayat` int(11) NOT NULL,
+  `waktu` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_lokasi` int(11) NOT NULL,
+  `latitude_now` varchar(255) NOT NULL,
+  `longitude_now` varchar(255) NOT NULL,
+  `jarak_now` double NOT NULL,
+  `status` enum('di jalan','sudah sampai') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `riwayat`
+--
+
+INSERT INTO `riwayat` (`id_riwayat`, `waktu`, `id_lokasi`, `latitude_now`, `longitude_now`, `jarak_now`, `status`) VALUES
+(1, '2020-09-24 06:39:27', 2, '5', '2', 4, 'di jalan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kendaraan`
+--
+
+CREATE TABLE `tb_kendaraan` (
+  `id_kendaraan` int(11) NOT NULL,
+  `jenis_kendaraan` varchar(255) NOT NULL,
+  `merk_kendaraan` varchar(255) NOT NULL,
+  `nomor_kendaraan` varchar(255) NOT NULL,
+  `pengguna_kendaraan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_kendaraan`
+--
+
+INSERT INTO `tb_kendaraan` (`id_kendaraan`, `jenis_kendaraan`, `merk_kendaraan`, `nomor_kendaraan`, `pengguna_kendaraan`) VALUES
+(1, 'mobil', 'toyota', 'p 3047 ko', 'Sari'),
+(2, 'mobil', 'toyota', 'P 4301 MD', 'Ella'),
+(3, 'mobil', 'toyota', 'P 2076 YT', 'Sari'),
+(4, 'mobil', 'toyota', 'P 5628 PT', 'Nando');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(255) NOT NULL,
+  `user_admin` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_admin`, `nama_admin`, `user_admin`, `password`, `email`, `level`) VALUES
+(1, 'sari', 'sari', 'sari', 'sariirkh@gmail.com', 'admin'),
+(2, 'Ella why', 'ella', 'ella', 'Ellawhy@gmail.com', 'admin'),
+(6, 'HRD', 'hrd', 'hrd', 'hrd@gmail.com', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `lokasi`
+--
+ALTER TABLE `lokasi`
+  ADD PRIMARY KEY (`id_lokasi`);
+
+--
+-- Indexes for table `tb_kendaraan`
+--
+ALTER TABLE `tb_kendaraan`
+  ADD PRIMARY KEY (`id_kendaraan`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `lokasi`
+--
+ALTER TABLE `lokasi`
+  MODIFY `id_lokasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `tb_kendaraan`
+--
+ALTER TABLE `tb_kendaraan`
+  MODIFY `id_kendaraan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
