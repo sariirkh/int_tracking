243d2e7b9936adc293acd2bf0bf4be3375dc3fdb
sariirkh <puspitasarirakhmatullah@gmail.com>
2020-09-28 15:48:22 +0700

    up

--- application/controllers/Kendaraan.php
+++ application/controllers/Kendaraan.php
@@ -18,10 +18,15 @@ class Kendaraan extends CI_Controller
 	var $mainTable="tb_kendaraan";
 	var $mainPk="id_kendaraan";
 	var $viewLink="Kendaraan";
+	// var $viewLink2="Users";
+	//sub menu atau header
 	var $breadcrumbTitle="Daftar Kendaraan";
+	//var $breadcrumbTitle2="User Access";
+	// buat tampilan view data
 	var $viewPage="Admviewpage";
+	//buat view tambah data
 	var $addPage="Admaddpage";
-	
+	//var $detPage="Formdetpage";
 	
 	//query
 	var $ordQuery=" ORDER BY id_kendaraan DESC ";
@@ -29,17 +34,19 @@ class Kendaraan extends CI_Controller
 						tb_kendaraan
 						";
 	var $fieldQuery="
-	id_kendaraan,
-	jenis_kendaraan,
-	merk_kendaraan,
-	nomor_kendaraan,
-	pengguna_kendaraan
+						id_kendaraan,
+						jenis_kendaraan,
+						merk_kendaraan,
+						nomor_kendaraan,
+						pengguna_kendaraan
 						"; //leave blank to show all field
 						
 	var $primaryKey="id_kendaraan";
+	//var $detKey="nik";
 	var $updateKey="id_kendaraan";
 	
 	//auto generate id
+	//sesuaikan panjangnya length di database
 	var $defaultId="KDR0001";
 	var $prefix="KDR";
 	var $suffix="0001";	
@@ -47,21 +54,21 @@ class Kendaraan extends CI_Controller
 	//view
 	var $viewFormTitle="Daftar Kendaraan";
 	var $viewFormTableHeader=array(
-		"Id Kendaraan",
-		"Jenis Kendaraan",
-		"Merk Kendaraan",
-		"Nomor Kendaraan",
-		"Nama Pengguna"
+									"Id Kendaraan",
+									"Jenis Kendaraan",
+									"Merk Kendaraan",
+									"Nomor Kendaraan",
+									"Nama Pengguna"
 									);
 	
 	//save
 	var $saveFormTitle="Tambah Kendaraan";
 	var $saveFormTableHeader=array(
-		"Id Kendaraan",
-		"Jenis Kendaraan",
-		"Merk Kendaraan",
-		"Nomor Kendaraan",
-		"Nama Pengguna"
+									"Id Kendaraan",
+									"Jenis Kendaraan",
+									"Merk Kendaraan",
+									"Nomor Kendaraan",
+									"Nama Pengguna"
 									);
 	
 	//update
@@ -86,7 +93,7 @@ class Kendaraan extends CI_Controller
 		{
 			$access=$row;
 		}
-		
+		//$selfDept=$this->Mmain->qRead("tb_emp WHERE id_emp='".$this->session->userdata('idEmp')."'","id_div,id_loc","")->row();
 		
 		//$output['isall']=$access->isadd;
 		$accessQuery="";
@@ -118,8 +125,7 @@ class Kendaraan extends CI_Controller
 		$renderTemp=$this->Mmain->qRead($this->tableQuery.$this->ordQuery,$this->fieldQuery,"");
 		foreach($renderTemp->result() as $row)
 		{
-			
-			
+
 		}
 		$output['render']=$renderTemp;
 		//init view
@@ -129,8 +135,9 @@ class Kendaraan extends CI_Controller
 		$output['saveLink']=$this->viewLink."/add";
 		$output['deleteLink']=$this->viewLink."/delete";
 		$output['primaryKey']=$this->primaryKey;
+		//$output['detKey']=$this->detKey;
 		$output['tableHeader']=$this->viewFormTableHeader;
-		
+		//$output['dtcustom']="datatableemp";
 		
 		//render view
 		$this->fn->getheader();
@@ -165,8 +172,7 @@ class Kendaraan extends CI_Controller
 			$output['saveLink']=$this->viewLink."/update";
 			$pid=$isEdit;
 			
-						
-			$render=$this->Mmain->qRead($this->tableQuery,$this->fieldQuery,$This->mainPK."  = '".$isEdit."'");
+			$render=$this->Mmain->qRead($this->tableQuery,$this->fieldQuery,$this->mainPk."  = '".$isEdit."'");
 			foreach($render->result() as $row)
 			{
 				foreach($row as $col)
@@ -175,8 +181,6 @@ class Kendaraan extends CI_Controller
 				}
 			}
 			
-				
-				
 		}
 		else
 		{	
@@ -186,23 +190,23 @@ class Kendaraan extends CI_Controller
 				}	
 				
 				//generate id
-				$txtVal[0]=$this->Mmain->autoId($this->mainTable,$This->mainPK,$this->prefix,$this->defaultId,$this->suffix);	
+				$txtVal[0]=$this->Mmain->autoId($this->mainTable,$this->mainPk,$this->prefix,$this->defaultId,$this->suffix);	
 				
 	
 		}
 		
-		//$cboacc=$this->fn->createCbofromDb("tb_acc","id_acc as id,nm_acc as nm","",$txtVal[58],"","txtUser[]");
-			//$cboBlood=$this->fn->createCbo(array('A','B','O','AB','-'),array('A','B','O','AB','-'),$txtVal[29]);
-		
+		// $cboacc=$this->fn->createCbofromDb("tb_acc","id_acc as id,nm_acc as nm","",$txtVal[58],"","txtUser[]");
+		// $cboBlood=$this->fn->createCbo(array('A','B','O','AB','-'),array('A','B','O','AB','-'),$txtVal[29]);
 		
 		
 		$output['formTxt']=array(
-			"<input type='text' class='form-control' id='txtIdKendaraan' name=txt[] value='".$txtVal[0]."' required readonly placeholder='Max. 70 karakter' maxlength='70'>",
-			"<input type='text' class='form-control' id='txtJenisKendaraan' name=txt[] value='".$txtVal[1]."' required placeholder='Max. 70 karakter' maxlength='70'>",
-			"<input type='text' class='form-control' id='txtMerkKendaraan' name=txt[] value='".$txtVal[2]."' required placeholder='Max. 70 karakter' maxlength='70'>",
-			"<input type='text' class='form-control' id='txtNomorKendaraan' name=txt[] value='".$txtVal[3]."' required placeholder='Max. 70 karakter' maxlength='70'>",
-			"<input type='text' class='form-control' id='txtPengguna' name=txt[] value='".$txtVal[4]."' required placeholder='Max. 70 karakter' maxlength='70'>"
-			);
+								"<input type='text' class='form-control' id='txtIdKendaraan' name=txt[] value='".$txtVal[0]."' required readonly placeholder='Max. 70 karakter' maxlength='70'>",
+								"<input type='text' class='form-control' id='txtJenisKendaraan' name=txt[] value='".$txtVal[1]."' required placeholder='Max. 70 karakter' maxlength='70'>",
+								"<input type='text' class='form-control' id='txtMerkKendaraan' name=txt[] value='".$txtVal[2]."' required placeholder='Max. 70 karakter' maxlength='70'>",
+								"<input type='text' class='form-control' id='txtNomorKendaraan' name=txt[] value='".$txtVal[3]."' required placeholder='Max. 70 karakter' maxlength='70'>",
+								"<input type='text' class='form-control' id='txtPengguna' name=txt[] value='".$txtVal[4]."' required placeholder='Max. 70 karakter' maxlength='70'>"
+								
+								);
 		
 		
 		//load view
@@ -215,7 +219,6 @@ class Kendaraan extends CI_Controller
 	{
 		//retrieve values
 		$savValTemp=$this->input->post('txt');
-		$savValUserTemp=$this->input->post('txtUser');
 		
 		//save to database
 		$this->load->database();
@@ -236,7 +239,6 @@ class Kendaraan extends CI_Controller
 		$this->load->model('Mmain');
 		$this->Mmain->qDel($this->mainTable,$this->mainPk,$valId);
 		
-		
 		$this->session->set_flashdata('successNotification', '3');
 		//redirect to form
 		redirect($this->viewLink,'refresh');		
@@ -248,49 +250,40 @@ class Kendaraan extends CI_Controller
 		//retrieve values
 		$savValTemp=$this->input->post('txt');
 		
-		
 		//save to database
 		$this->load->database();
 		$this->load->model('Mmain');
-		$avauser="";
-		if(!empty($_FILES['txtfl']['name']))
-		{
-			$flName=$_FILES['txtfl']['name'];
-			$flTmp=$_FILES['txtfl']['tmp_name'];
-			$fltype=$_FILES['txtfl']['type'];
-			move_uploaded_file($flTmp,"assets/admin/img/avatar/thumb/".$flName);
-			$avauser=$flName;
-		}
-		else
-		{
-			
+		// $avauser="";
+		// if(!empty($_FILES['txtfl']['name']))
+		// {
+		// 	$flName=$_FILES['txtfl']['name'];
+		// 	$flTmp=$_FILES['txtfl']['tmp_name'];
+		// 	$fltype=$_FILES['txtfl']['type'];
+		// 	move_uploaded_file($flTmp,"assets/admin/img/avatar/thumb/".$flName);
+		// 	$avauser=$flName;
+		// }
+		// else
+		// {
+		// 	$avauser=$this->input->post('txtimg');
+		// }
 		
-		$this->Mmain->qUpd("$This->maintable $This->mainPk",$savValTemp[0],$savValTemp);
 		
 		
-		$this->Mmain->qUpdpart("tb_user","code_user",$savValUserTemp[0],Array("ava_user","id_acc","nm_user"),Array($avauser,$savValUserTemp[3],$savValUserTemp[1]));
+		
+		// $savValTemp[] = $savValUserTemp[0];
+		
+		
+		// //foreach($savValTemp as $i => $row) echo ($i+1)." ".$row."<br>";
+		// //foreach($savValUserTemp as $i => $row) echo ($i+1)." ".$row."<br>";
+		
+		$this->Mmain->qUpd($this->mainTable,$this->mainPk,$savValTemp[0],$savValTemp);
 		
 		$this->session->set_flashdata('successNotification', '2');
 		//redirect to form
 		redirect($this->viewLink,'refresh');		
-		}
-	
-	
-	//update record
-	// public function Pin($id,$stat)
-	// {
-	// 	//retrieve values
-		
-		
-	// 	//save to database
-	// 	$this->load->database();
-	// 	$this->load->model('Mmain');
-	// 	$this->Mmain->qUpdpart($this->mainTable,"id_emp",$id,Array("show_emp"),Array($stat));
-		
-	// 	//redirect to form
-	// 	redirect($this->viewLink,'refresh');		
-		
 	}
+	
+	
 }
 
 ?>
--- application/controllers/Request_rute.php
+++ application/controllers/Request_rute.php
@@ -15,57 +15,64 @@ class Request_rute extends CI_Controller
 		====================================================== Variable Declaration =========================================================
 	*/
 	
-	var $mainTable="tb_kendaraan";
-	var $mainPk="id_kendaraan";
-	var $viewLink="Kendaraan";
-	var $breadcrumbTitle="Daftar Kendaraan";
+	var $mainTable="tb_lokasi";
+	var $mainPk="id_lokasi";
+	var $viewLink="Request Rute";
+	// var $viewLink2="Users";
+	//sub menu atau header
+	var $breadcrumbTitle="Data Request Rute";
+	//var $breadcrumbTitle2="User Access";
+	// buat tampilan view data
 	var $viewPage="Admviewpage";
+	//buat view tambah data
 	var $addPage="Admaddpage";
-	
+	//var $detPage="Formdetpage";
 	
 	//query
-	var $ordQuery=" ORDER BY id_kendaraan DESC ";
-	var $tableQuery="
-						tb_kendaraan
-						";
-	var $fieldQuery="
-	id_kendaraan,
-	jenis_kendaraan,
-	merk_kendaraan,
-	nomor_kendaraan,
-	pengguna_kendaraan
-						"; //leave blank to show all field
+	var $ordQuery=" ORDER BY id_lokasi DESC ";
+	var $tableQuery="tb_lokasi AS a INNER JOIN tb_kendaraan AS b ON a.id_lokasi = b.id_kendaraan";
+	var $fieldQuery=" a.code_frm as code,a.id_frm as id,a.desc_frm as nm,b.nm_frmgroup as grp, a.is_shortcut as sc, a.stat_frm as st, a.sort_order"; //leave blank to show all field
+	
+	// var $tableQuery="
+	// 					tb_kendaraan
+	// 					";
+	// var $fieldQuery="
+	// 					id_lokasi,
+	// 					id_kendaraan,
+	// 					nama_lokasi,
+	// 					"; //leave blank to show all field
 						
-	var $primaryKey="id_kendaraan";
-	var $updateKey="id_kendaraan";
+	var $primaryKey="id_lokasi";
+	//var $detKey="nik";
+	var $updateKey="id_lokasi";
 	
 	//auto generate id
-	var $defaultId="KDR0001";
-	var $prefix="KDR";
-	var $suffix="0001";	
+	//sesuaikan panjangnya length di database
+	var $defaultId="LKS0001";
+	var $prefix="LKS";
+	var $suffix="00001";	
 	
 	//view
-	var $viewFormTitle="Daftar Kendaraan";
+	var $viewFormTitle="Daftar Request Rute";
 	var $viewFormTableHeader=array(
-		"Id Kendaraan",
-		"Jenis Kendaraan",
-		"Merk Kendaraan",
-		"Nomor Kendaraan",
-		"Nama Pengguna"
+									"Id Lokasi",
+									"Id Kendaraan",
+									"Kendaraan",
+									"Pengguna",
+									"Lokasi"
 									);
 	
 	//save
-	var $saveFormTitle="Tambah Kendaraan";
+	var $saveFormTitle="Tambah Rute";
 	var $saveFormTableHeader=array(
-		"Id Kendaraan",
-		"Jenis Kendaraan",
-		"Merk Kendaraan",
-		"Nomor Kendaraan",
-		"Nama Pengguna"
+									"Id Lokasi",
+									"Id Kendaraan",
+									"Pengguna",
+									"Lokasi"
 									);
 	
 	//update
-	var $editFormTitle="Ubah Data Kendaraan";
+	var $editFormTitle="Ubah Rute";
 	
 	/*	
 		========================================================== General Function =========================================================
@@ -86,7 +93,7 @@ class Request_rute extends CI_Controller
 		{
 			$access=$row;
 		}
-		
+		//$selfDept=$this->Mmain->qRead("tb_emp WHERE id_emp='".$this->session->userdata('idEmp')."'","id_div,id_loc","")->row();
 		
 		//$output['isall']=$access->isadd;
 		$accessQuery="";
@@ -118,8 +125,7 @@ class Request_rute extends CI_Controller
 		$renderTemp=$this->Mmain->qRead($this->tableQuery.$this->ordQuery,$this->fieldQuery,"");
 		foreach($renderTemp->result() as $row)
 		{
-			
-			
+
 		}
 		$output['render']=$renderTemp;
 		//init view
@@ -129,8 +135,9 @@ class Request_rute extends CI_Controller
 		$output['saveLink']=$this->viewLink."/add";
 		$output['deleteLink']=$this->viewLink."/delete";
 		$output['primaryKey']=$this->primaryKey;
+		//$output['detKey']=$this->detKey;
 		$output['tableHeader']=$this->viewFormTableHeader;
-		
+		//$output['dtcustom']="datatableemp";
 		
 		//render view
 		$this->fn->getheader();
@@ -165,8 +172,7 @@ class Request_rute extends CI_Controller
 			$output['saveLink']=$this->viewLink."/update";
 			$pid=$isEdit;
 			
-						
-			$render=$this->Mmain->qRead($this->tableQuery,$this->fieldQuery,$This->mainPK."  = '".$isEdit."'");
+			$render=$this->Mmain->qRead($this->tableQuery,$this->fieldQuery,$this->mainPk."  = '".$isEdit."'");
 			foreach($render->result() as $row)
 			{
 				foreach($row as $col)
@@ -175,8 +181,6 @@ class Request_rute extends CI_Controller
 				}
 			}
 			
-				
-				
 		}
 		else
 		{	
@@ -186,23 +190,22 @@ class Request_rute extends CI_Controller
 				}	
 				
 				//generate id
-				$txtVal[0]=$this->Mmain->autoId($this->mainTable,$This->mainPK,$this->prefix,$this->defaultId,$this->suffix);	
+				$txtVal[0]=$this->Mmain->autoId($this->mainTable,$this->mainPk,$this->prefix,$this->defaultId,$this->suffix);	
 				
 	
 		}
 		
-		//$cboacc=$this->fn->createCbofromDb("tb_acc","id_acc as id,nm_acc as nm","",$txtVal[58],"","txtUser[]");
-			//$cboBlood=$this->fn->createCbo(array('A','B','O','AB','-'),array('A','B','O','AB','-'),$txtVal[29]);
-		
+		// $cboacc=$this->fn->createCbofromDb("tb_acc","id_acc as id,nm_acc as nm","",$txtVal[58],"","txtUser[]");
+		// $cboBlood=$this->fn->createCbo(array('A','B','O','AB','-'),array('A','B','O','AB','-'),$txtVal[29]);
 		
 		
 		$output['formTxt']=array(
-			"<input type='text' class='form-control' id='txtIdKendaraan' name=txt[] value='".$txtVal[0]."' required readonly placeholder='Max. 70 karakter' maxlength='70'>",
-			"<input type='text' class='form-control' id='txtJenisKendaraan' name=txt[] value='".$txtVal[1]."' required placeholder='Max. 70 karakter' maxlength='70'>",
-			"<input type='text' class='form-control' id='txtMerkKendaraan' name=txt[] value='".$txtVal[2]."' required placeholder='Max. 70 karakter' maxlength='70'>",
-			"<input type='text' class='form-control' id='txtNomorKendaraan' name=txt[] value='".$txtVal[3]."' required placeholder='Max. 70 karakter' maxlength='70'>",
-			"<input type='text' class='form-control' id='txtPengguna' name=txt[] value='".$txtVal[4]."' required placeholder='Max. 70 karakter' maxlength='70'>"
-			);
+								"<input type='text' class='form-control' id='txtIdLokasi' name=txt[] value='".$txtVal[0]."' required readonly placeholder='Max. 70 karakter' maxlength='70'>",
+								"<input type='text' class='form-control' id='txtKendaraan' name=txt[] value='".$txtVal[1]."' required placeholder='Max. 70 karakter' maxlength='70'>",
+								"<input type='text' class='form-control' id='txtPengguna' name=txt[] value='".$txtVal[2]."' required placeholder='Max. 70 karakter' maxlength='70'>",
+								"<input type='text' class='form-control' id='txtLokasi' name=txt[] value='".$txtVal[3]."' required placeholder='Max. 70 karakter' maxlength='70'>",
+								
+								);
 		
 		
 		//load view
@@ -215,7 +218,6 @@ class Request_rute extends CI_Controller
 	{
 		//retrieve values
 		$savValTemp=$this->input->post('txt');
-		$savValUserTemp=$this->input->post('txtUser');
 		
 		//save to database
 		$this->load->database();
@@ -236,7 +238,6 @@ class Request_rute extends CI_Controller
 		$this->load->model('Mmain');
 		$this->Mmain->qDel($this->mainTable,$this->mainPk,$valId);
 		
-		
 		$this->session->set_flashdata('successNotification', '3');
 		//redirect to form
 		redirect($this->viewLink,'refresh');		
@@ -248,49 +249,40 @@ class Request_rute extends CI_Controller
 		//retrieve values
 		$savValTemp=$this->input->post('txt');
 		
-		
 		//save to database
 		$this->load->database();
 		$this->load->model('Mmain');
-		$avauser="";
-		if(!empty($_FILES['txtfl']['name']))
-		{
-			$flName=$_FILES['txtfl']['name'];
-			$flTmp=$_FILES['txtfl']['tmp_name'];
-			$fltype=$_FILES['txtfl']['type'];
-			move_uploaded_file($flTmp,"assets/admin/img/avatar/thumb/".$flName);
-			$avauser=$flName;
-		}
-		else
-		{
-			
+		// $avauser="";
+		// if(!empty($_FILES['txtfl']['name']))
+		// {
+		// 	$flName=$_FILES['txtfl']['name'];
+		// 	$flTmp=$_FILES['txtfl']['tmp_name'];
+		// 	$fltype=$_FILES['txtfl']['type'];
+		// 	move_uploaded_file($flTmp,"assets/admin/img/avatar/thumb/".$flName);
+		// 	$avauser=$flName;
+		// }
+		// else
+		// {
+		// 	$avauser=$this->input->post('txtimg');
+		// }
 		
-		$this->Mmain->qUpd("$This->maintable $This->mainPk",$savValTemp[0],$savValTemp);
 		
 		
-		$this->Mmain->qUpdpart("tb_user","code_user",$savValUserTemp[0],Array("ava_user","id_acc","nm_user"),Array($avauser,$savValUserTemp[3],$savValUserTemp[1]));
+		
+		// $savValTemp[] = $savValUserTemp[0];
+		
+		
+		// //foreach($savValTemp as $i => $row) echo ($i+1)." ".$row."<br>";
+		// //foreach($savValUserTemp as $i => $row) echo ($i+1)." ".$row."<br>";
+		
+		$this->Mmain->qUpd($this->mainTable,$this->mainPk,$savValTemp[0],$savValTemp);
 		
 		$this->session->set_flashdata('successNotification', '2');
 		//redirect to form
 		redirect($this->viewLink,'refresh');		
-		}
-	
-	
-	//update record
-	// public function Pin($id,$stat)
-	// {
-	// 	//retrieve values
-		
-		
-	// 	//save to database
-	// 	$this->load->database();
-	// 	$this->load->model('Mmain');
-	// 	$this->Mmain->qUpdpart($this->mainTable,"id_emp",$id,Array("show_emp"),Array($stat));
-		
-	// 	//redirect to form
-	// 	redirect($this->viewLink,'refresh');		
-		
 	}
+	
+	
 }
 
 ?>
