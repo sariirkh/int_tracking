bbed28cb589e15ddc2cf3867a8b2bab219c50209
sariirkh <puspitasarirakhmatullah@gmail.com>
2020-10-06 11:51:23 +0700

    up

--- application/views/map/index.php
+++ application/views/map/index.php
@@ -35,17 +35,18 @@
      
     // needed token
     ACCESS_TOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ';
-ACCESS_TOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw';
+    ACCESS_TOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw';
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + ACCESS_TOKEN, {
         attribution: 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
         id: 'mapbox.streets'
     }).addTo(map); 
 
-    //buat titik
+    //buat titik lokasi
     var marker = L.marker([-8.203184,113.571038]).addTo(map);
-marker.bindPopup('<b>PT. Mangli Djaya Raya</b><br>JL Mayjend DI Panjaitan No.99, Krajan, Petung, Kec. Bangsalsari, Kabupaten Jember, Jawa Timur 68154');
+    marker.bindPopup('<b>PT. Mangli Djaya Raya</b><br>JL Mayjend DI Panjaitan No.99, Krajan, Petung, Kec. Bangsalsari, Kabupaten Jember, Jawa Timur 68154');
 
-map.on('click', function (e) {
+    //buat ambil lat, long
+    map.on('click', function (e) {
     alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng);
 });
 </script>
