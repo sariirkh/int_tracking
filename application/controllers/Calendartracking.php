1ba3ab5a81a671d4c4e2d2110848198660746611
ellawahyuningtyas <58216042+ellawahyuningtyas@users.noreply.github.com>
2020-11-04 16:38:10 -1200

    kalender

--- application/controllers/Calendartracking.php
+++ application/controllers/Calendartracking.php
@@ -49,8 +49,29 @@ class Calendartracking extends CI_Controller
      	
     }
     
-   
+	public function getKegiatan()
+	{
+		//init modal
+		$this->load->database();
+		$this->load->model('Mmain');
+		
+		$kalenderQuery = " tb_lokasi a
+							INNER JOIN tb_kendaraan b ON a.id_kendaraan = b.id_kendaraan ";
+		$kalenderField = "  a.nama_lokasi as nm,a.tanggal as tgl, CONCAT(nama_kendaraan,' ',nomor_kendaraan) as ken";
+		
+		$returnValue="";
+		$renderTemp=$this->Mmain->qRead($kalenderQuery,$kalenderField,"");	
+		foreach($renderTemp->result() as $row)
+		{
+			$returnValue .= $row->nm . "||" .$row->tgl . "||" . $row->ken . "++";
+		}
+		
+		echo $returnValue;
+	}
+
+
     
 }
 ?>
 
+	
--- application/views/adm_footer.php
+++ application/views/adm_footer.php
@@ -65,21 +65,6 @@ foreach($setting ->result() as $row)
 <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.2.0/js/dataTables.responsive.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/select/1.3.0/js/dataTables.select.min.js"></script>
 
-<!-- css calendar -->
-<link rel="stylesheet" href="<?php echo base_url();?>assets/landing/plugins/fullcalendar/main.min.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/landing/plugins/fullcalendar-interaction/main.min.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/landing/plugins/fullcalendar-daygrid/main.min.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/landing/plugins/fullcalendar-timegrid/main.min.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/landing/plugins/fullcalendar-bootstrap/main.min.css">
-
-<!-- JQcalendar -->
-<script src="<?php echo base_url();?>assets/landing/plugins/moment/moment.min.js"></script>
-<script src="<?php echo base_url();?>assets/landing/plugins/fullcalendar/main.min.js"></script>
-<script src="<?php echo base_url();?>assets/landing/plugins/fullcalendar-daygrid/main.min.js"></script>
-<script src="<?php echo base_url();?>assets/landing/plugins/fullcalendar-timegrid/main.min.js"></script>
-<script src="<?php echo base_url();?>assets/landing/plugins/fullcalendar-interaction/main.min.js"></script>
-<script src="<?php echo base_url();?>assets/landing/plugins/fullcalendar-bootstrap/main.min.js"></script>
-
 <!-- JQVMap -->
 <script src="<?php echo base_url();?>assets/landing/plugins/jqvmap/jquery.vmap.min.js"></script>
 <script src="<?php echo base_url();?>assets/landing/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
@@ -707,8 +692,81 @@ $.ajax({
 	
 }
 </script>
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
+	
+	
 
-<script>
+	 <script type="text/javascript">
+	// alert();
+	 try
+	 {
+		 var kegiatan = [];
+		 
+		// kegiatan[0] = { "title"  : 'event1', "start"  : '2019-02-01' , "end" : "2019-02-05"};
+		// kegiatan[1] = { "title"  : 'event2', "start"  : '2019-02-03'};
+		 
+		 
+		 $.ajax({
+			url : "<?= site_url();?>Calendartracking/getKegiatan",
+			async : false,
+			success : function(s){
+				//alert(s);
+				if(s != "")
+				{
+					var dt = s.split("++");
+					
+					for(var i=0;i<dt.length;i++)
+					{
+						if(dt[i] != "")
+						{
+							var detail = dt[i].split("||");
+							//alert(detail);
+								kegiatan[i] = { "title"  : detail[0],
+											 "start"  : detail[1] , 
+											 "end" : detail[1] ,
+											  "description" : detail[2]};
+							
+						}
+					}
+				}
+				
+			}
+		 });
+		 
+		 $('#calendar').fullCalendar({
+			 weekNumbers:true,
+			selectable: true,
+			header: {
+			  left: 'prev,next today',
+			  center: 'title',
+			  right: 'month,agendaWeek,agendaDay'
+			},
+			dayClick: function(date) {
+			  //alert('clicked ' + date.format());
+			},
+			select: function(startDate, endDate) {
+			  //alert('selected ' + startDate.format() + ' to ' + endDate.format());
+			},
+			eventRender: function(eventObj, $el) {
+			  $el.popover({
+				title: eventObj.title,
+				content: eventObj.description,
+				trigger: 'click',
+				placement: 'top',
+				container: 'body'
+			  });
+			},
+			 events: kegiatan
+		  });
+			
+	 }
+	 catch(e)
+	 {
+		 alert(e.message);
+	 }
+	 </script>
+<!-- <script>
   $(function () {
 
     /* initialize the external events
@@ -875,7 +933,7 @@ $.ajax({
       $('#new-event').val('')
     })
   })
-</script>
+</script> -->
 
 
 <script type="text/javascript">
